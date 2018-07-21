30885891f3b3fba8deb760d2b970408ae794476e
Alonso Ortega <alonsojortega@gmail.com>
2018-07-21 06:47:26 +0000

    add mongolab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
     
-mongoose.connect("mongodb://localhost/yelp_camp_deploy");
+//mongoose.connect("mongodb://localhost/yelp_camp_deploy");
+mongoose.connect("mongodb://alonso:nicole1@ds145951.mlab.com:45951/alonsoyelpcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
